c174d1155051e5b078677ab8c8c29f46acbb6b94
Hugo SALOU <y.hugo.s29@gmail.com>
2022-02-08 17:30:33 +0100

    2022 goals

--- components/math.js
+++
@@ -1,7 +0,0 @@
-import { InlineMath, BlockMath } from 'react-katex';
-
-export const VecI = () => <InlineMath math={'\\vec{\\imath}'} />;
-export const VecJ = () => <InlineMath math={'\\vec{\\jmath}'} />;
-export const LaTeX = () => <InlineMath math={'\\LaTeX'} />;
-export const BaselFormula = () => 
-	<BlockMath  math={'\\sum_{n=1}^{\\infty} \\frac{1}{n^2} = \\frac{\\pi^2}{6}'} />;
--- components/redirect.js
+++
@@ -1,9 +0,0 @@
-function Redirect({ to }) {
-  if (typeof window != 'undefined') {
-    window.location.replace(to);
-  }
-
-  return <></>;
-}
-
-export default Redirect;
